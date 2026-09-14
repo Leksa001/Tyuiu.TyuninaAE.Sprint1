1920f74b2f969e4e8924dc4a8ba453bd0bc92387
Tyunina_Alexsandra <281037+tyunina_alexsandra@users.no-reply.gitverse.ru>
2026-09-15 01:04:53 +0500

    Task1

---
+++ Tyuiu.TyuninaAE.Sprint1.Task1.V26.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.TyuninaAE.Sprint1.Task1.V26.Lib;
+
+namespace Tyuiu.TyuninaAE.Sprint1.Task1.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(1, 2);
+            Assert.AreEqual(1.25, res);
+        }
+    }
+}
---
+++ Tyuiu.TyuninaAE.Sprint1.Task1.V26/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Tyuiu.TyuninaAE.Sprint1.Task1.V26.Lib;
+
+namespace Tyuiu.TyuninaAE.Sprint1.Task1.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Тюнина А. Е. | ИБКСб-26-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Организация ввода\\вывода в консольных приложениях                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Тюнина Александра Евгеньевна | ИБКСб-26-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные         *");
+            Console.WriteLine("* данные, вычисляет результат по формуле 5*x/(2+y) и печатает его         *");
+            Console.WriteLine("* на экране.                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("5*x/(2+y) = " + ds.Calculate(x, y));
+
+            Console.ReadKey();
+        }
+    }
+}
