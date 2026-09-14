a3ae210bcfdcd0f91670d6b8932aed70fb92d17e
Tyunina_Alexsandra <281037+tyunina_alexsandra@users.no-reply.gitverse.ru>
2026-09-15 00:19:29 +0500

    Task0

--- Tyuiu.TyuninaAE.Sprint1.Task0.V9.Lib/DataService.cs
+++ Tyuiu.TyuninaAE.Sprint1.Task0.V9.Lib/DataService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using tyuiu.cources.programming.interfaces.Sprint1;
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
 
 namespace Tyuiu.TyuninaAE.Sprint1.Task0.V9.Lib
 {
