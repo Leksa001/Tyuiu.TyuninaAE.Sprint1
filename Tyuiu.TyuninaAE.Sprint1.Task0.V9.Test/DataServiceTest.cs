07760425555d275096d9e707fe8f1593f1040ee2
Tyunina_Alexsandra <281037+tyunina_alexsandra@users.no-reply.gitverse.ru>
2026-09-15 00:16:30 +0500

    Task0

---
+++ Tyuiu.TyuninaAE.Sprint1.Task0.V9.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.TyuninaAE.Sprint1.Task0.V9.Lib;
+
+namespace Tyuiu.TyuninaAE.Sprint1.Task0.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(3, res);
+        }
+    }
+}
