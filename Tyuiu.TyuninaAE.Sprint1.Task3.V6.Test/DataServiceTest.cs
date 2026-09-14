45a0a44cd9df551164ea48a5fea46c7b0b48f370
Tyunina_Alexsandra <281037+tyunina_alexsandra@users.no-reply.gitverse.ru>
2026-09-15 01:34:48 +0500

    Task3

---
+++ Tyuiu.TyuninaAE.Sprint1.Task3.V6.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.TyuninaAE.Sprint1.Task3.V6.Lib;
+
+namespace Tyuiu.TyuninaAE.Sprint1.Task3.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double res = ds.TravelCost(67, 8.5, 6.5);
+            Assert.AreEqual(74.035, res);
+        }
+    }
+}
