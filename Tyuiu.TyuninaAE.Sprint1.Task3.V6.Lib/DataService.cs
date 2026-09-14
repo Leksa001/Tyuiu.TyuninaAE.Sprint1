75862a0eb302f68b0ef7277ba4fcf5ca0d6b2eb0
Leksa001 <206306046+Leksa001@users.noreply.github.com>
2026-09-15 01:34:48 +0500

    Task3

---
+++ Tyuiu.TyuninaAE.Sprint1.Task3.V6.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.TyuninaAE.Sprint1.Task3.V6.Lib
+{
+    public class DataService : ISprint1Task3V6
+    {
+        public double TravelCost(double distance, double gasFlow, double gasPrice)
+        {
+            double cost = 2 * distance * gasFlow / 100 * gasPrice;
+            return Math.Round(cost, 3);
+        }
+    }
+}
---
+++ Tyuiu.TyuninaAE.Sprint1.Task3.V6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Tyuiu.TyuninaAE.Sprint1.Task3.V6.Lib;
+
+namespace Tyuiu.TyuninaAE.Sprint1.Task3.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Тюнина А. Е. | ИБКСб-26-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #6                                                              *");
+            Console.WriteLine("* Выполнил: Тюнина Александра Евгеньевна | ИБКСб-26-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Вычислить стоимость поездки на автомобиле на дачу (туда и обратно).     *");
+            Console.WriteLine("* Исходные данные: расстояние до дачи (км), расход бензина на 100 км,     *");
+            Console.WriteLine("* цена одного литра бензина. Ответ округлить до 3 знаков после запятой.   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Расстояние до дачи (км) -> ");
+            double distance = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Расход бензина (литров на 100 км пробега) -> ");
+            double gasFlow = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Цена литра бензина (руб.) -> ");
+            double gasPrice = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Поездка на дачу и обратно обойдется в " + ds.TravelCost(distance, gasFlow, gasPrice) + " руб.");
+
+            Console.ReadKey();
+        }
+    }
+}
