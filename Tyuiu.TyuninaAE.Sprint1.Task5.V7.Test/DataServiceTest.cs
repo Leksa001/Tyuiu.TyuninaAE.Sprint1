87b3668b292db6b1346d0c824a8a582de8ca74ce
Tyunina_Alexsandra <281037+tyunina_alexsandra@users.no-reply.gitverse.ru>
2026-09-15 23:58:52 +0500

    Task5

---
+++ Tyuiu.TyuninaAE.Sprint1.Task5.V7.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.TyuninaAE.Sprint1.Task5.V7.Lib;
+
+namespace Tyuiu.TyuninaAE.Sprint1.Task5.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int res = ds.AngleToHoursMinutes(100.5);
+            Assert.AreEqual(3, res);
+        }
+    }
+}
