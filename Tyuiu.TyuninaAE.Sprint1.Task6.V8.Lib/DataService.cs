91c91bec174864005aeae55e9e59112f8082026e
Leksa001 <206306046+Leksa001@users.noreply.github.com>
2026-09-16 00:09:37 +0500

    Task6

---
+++ Tyuiu.TyuninaAE.Sprint1.Task6.V8.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.TyuninaAE.Sprint1.Task6.V8.Lib
+{
+    public class DataService : ISprint1Task6V8
+    {
+        public string MoveLetterToEnd(string value)
+        {
+            string[] words = value.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length > 1)
+                {
+                    words[i] = words[i].Substring(1) + words[i][0];
+                }
+            }
+            return string.Join(" ", words);
+        }
+    }
+}
