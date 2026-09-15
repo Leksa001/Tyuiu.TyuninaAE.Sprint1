cbf9fc9e38aff13d01d62f267dab23ec1aca83dd
Leksa001 <206306046+Leksa001@users.noreply.github.com>
2026-09-16 00:12:16 +0500

    Task7

---
+++ Tyuiu.TyuninaAE.Sprint1.Task7.V2.Lib/DataService.cs
@@ -0,0 +1,13 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.TyuninaAE.Sprint1.Task7.V2.Lib
+{
+    public class DataService : ISprint1Task7V2
+    {
+        public double Calculate(double x, double y)
+        {
+            double z = (Math.Sin(x) + Math.Cos(y)) / (Math.Cos(x) - Math.Sin(y)) * Math.Tan(x * y);
+            return Math.Round(z, 3);
+        }
+    }
+}
