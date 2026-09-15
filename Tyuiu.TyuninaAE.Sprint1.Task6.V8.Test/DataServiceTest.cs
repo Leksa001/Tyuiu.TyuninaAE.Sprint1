f3ecfbc38e3ecefe1b408a0d2de8b41dd3d06ac7
Tyunina_Alexsandra <281037+tyunina_alexsandra@users.no-reply.gitverse.ru>
2026-09-16 00:09:37 +0500

    Task6

---
+++ Tyuiu.TyuninaAE.Sprint1.Task6.V8.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.TyuninaAE.Sprint1.Task6.V8.Lib;
+
+namespace Tyuiu.TyuninaAE.Sprint1.Task6.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidString()
+        {
+            DataService ds = new DataService();
+            string res = ds.MoveLetterToEnd("Привет Мир");
+            string wait = "риветП ирМ";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.TyuninaAE.Sprint1.Task6.V8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Tyuiu.TyuninaAE.Sprint1.Task6.V8.Lib;
+
+namespace Tyuiu.TyuninaAE.Sprint1.Task6.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Тюнина А. Е. | ИБКСб-26-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Работа со строками класс String                                   *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнил: Тюнина Александра Евгеньевна | ИБКСб-26-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Пользователь вводит текст. Напечатать все слова, перенеся их первую     *");
+            Console.WriteLine("* букву в конец.                                                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.Write("* Введите текст: ");
+            string value = Console.ReadLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("* " + ds.MoveLetterToEnd(value));
+            Console.WriteLine("***************************************************************************");
+            Console.ReadKey();
+        }
+    }
+}
