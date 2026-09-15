877bbaac54b3d33dc9792567752791715881035b
Leksa001 <206306046+Leksa001@users.noreply.github.com>
2026-09-15 23:58:52 +0500

    Task5

---
+++ Tyuiu.TyuninaAE.Sprint1.Task5.V7.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.TyuninaAE.Sprint1.Task5.V7.Lib
+{
+    public class DataService : ISprint1Task5V7
+    {
+        public int AngleToHoursMinutes(double f)
+        {
+            return Convert.ToInt32(Math.Floor(f / 30));
+        }
+    }
+}
