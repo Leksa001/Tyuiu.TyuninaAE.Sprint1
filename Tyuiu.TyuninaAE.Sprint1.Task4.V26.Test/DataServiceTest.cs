47e562e0af05fa7dfcf066e3cd385c7f2629888a
Tyunina_Alexsandra <281037+tyunina_alexsandra@users.no-reply.gitverse.ru>
2026-09-15 22:11:09 +0500

    Task4

---
+++ Tyuiu.TyuninaAE.Sprint1.Task4.V26.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.TyuninaAE.Sprint1.Task4.V26.Lib;
+
+namespace Tyuiu.TyuninaAE.Sprint1.Task4.V26.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double res = ds.Calculate(1, -1);
+            Assert.AreEqual(-0.215, res);
+        }
+    }
+}
---
+++ Tyuiu.TyuninaAE.Sprint1.Task4.V26/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Tyuiu.TyuninaAE.Sprint1.Task4.V26.Lib;
+
+namespace Tyuiu.TyuninaAE.Sprint1.Task4.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Тюнина А. Е. | ИБКСб-26-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Class Math                                                        *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Тюнина Александра Евгеньевна | ИБКСб-26-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные         *");
+            Console.WriteLine("* данные, вычисляет результат по формуле (arctg(x) + y) / e^(y + x)       *");
+            Console.WriteLine("* и печатает его на экране. Ответ округлить до 3 знаков после запятой.    *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("(arctg(x) + y) / e^(y + x) = " + ds.Calculate(x, y));
+
+            Console.ReadKey();
+        }
+    }
+}
